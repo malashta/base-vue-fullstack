b7ff44d4d76c6d500723787b029877b7da08bf6b
Tallerr <malashta@gmail.com>
2018-02-05 20:01:08 +0700

    work environment

--- src/app.js
+++ src/app.js
@@ -7,3 +7,22 @@
 import 'babel-polyfill';
 import Vue from 'vue';
 import App from './App.vue';
+import {createStore} from './store';
+import {createRouter} from './router';
+import {sync} from 'vuex-router-sync';
+
+export function createApp() {
+  const store = createStore();
+  const router = createRouter();
+
+  sync(store, router);
+
+  const app = new Vue({
+    router,
+    store,
+    render: h => h(App)
+  });
+
+  return {app, router, store};
+}
+
